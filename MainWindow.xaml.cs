f2501ef66d4854d925b4e4065cb9fa29dd41888b
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-09-09 10:32:07 -0300

    feat: Arquivos C#

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Locomotiva
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var sb = (Storyboard)FindResource("AnimacaoLocomotiva");
+            sb.Begin(this, true);
+        }
+    }
+}
